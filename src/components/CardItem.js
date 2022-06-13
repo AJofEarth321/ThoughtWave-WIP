ee3fc3b6d1a407a8a052cc5268b33e850225d857
AJofEarth321 <103457615+AJofEarth321@users.noreply.github.com>
2022-06-12 22:16:48 -0400

    encounter a block regarding the cards component. Further inquiry required.

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -4,13 +4,19 @@ import { Link } from 'react-router-dom';
 function CardItem() {
   return (
     <>
-        <li className="cards_item">
-            <Link className="cards_item_link">
-                
+        <li className="cards__item">
+            <Link className="cards__item__link">
+               <figure className="cards__item__pic-wrap">
+                    <img src="/" alt=""
+                    className="cards__item_img"/>
+                </figure>
+                <div className='cards__item_info'>
+                    <h5 className='cards__item__text' />
+                </div>  
             </Link>
         </li>
     </>
-  )
+  );
 }
 
 export default CardItem
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../App.css';
+import Cards from '../components/Cards';
 import HeroSection from '../components/HeroSection';
 
 function Home () {
     return (
     <>
         <HeroSection />
+        {/* <Cards /> */}
     </>
     )
 }
