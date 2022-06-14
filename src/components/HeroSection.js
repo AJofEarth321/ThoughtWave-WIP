971b82c777ab25357576bec0627ae95b8b107ec4
AJofEarth321 <103457615+AJofEarth321@users.noreply.github.com>
2022-06-14 15:30:15 -0400

    resolve the hero container image/text issue by changing 'image' to 'img' in the css

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,12 +3,12 @@ import '../App.css';
 import { Button } from './Button'
 import './HeroSection.css';
 // import twHead from "../../public/assets/twHead.jpg";
-// import twHead from '../images/twHead.jpg';
+import twHead from '../images/twHead.jpg';
 
 function HeroSection() {
   return (
     <div className='hero-container'>
-        {/* <img src={twHead} alt="Logo" /> */}
+        <img src={twHead} alt="Logo" />
         <h1>Our Thoughts Matter</h1>
         <p>Join this amazing movement of the mind!</p>
         <div className="hero-btns">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,13 @@ import React from 'react';
 import '../App.css';
 import twHead from '../images/twHead.jpg';
 import Cards from '../components/Cards';
-// import HeroSection from '../components/HeroSection';
+import HeroSection from '../components/HeroSection';
 
 function Home () {
     return (
     <>
          {/* <img src={twHead} alt="Logo" /> */}
-        <h1>Test Text</h1>
-        {/* <HeroSection /> */}
+        <HeroSection />
         {/* <Cards /> */}
     </>
     )
