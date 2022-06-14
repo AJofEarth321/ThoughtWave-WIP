ef08e5169d43dad41a4e8ac7db9bcae299c423f8
AJofEarth321 <103457615+AJofEarth321@users.noreply.github.com>
2022-06-14 13:12:20 -0400

    center the mobile menu links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import '../App.css';
+import twHead from '../images/twHead.jpg';
 import Cards from '../components/Cards';
 // import HeroSection from '../components/HeroSection';
 
 function Home () {
     return (
     <>
-        <div>
-            <h1>Our Thoughts Matter.</h1>
-        </div>
+         {/* <img src={twHead} alt="Logo" /> */}
+        <h1>Test Text</h1>
         {/* <HeroSection /> */}
         {/* <Cards /> */}
     </>
