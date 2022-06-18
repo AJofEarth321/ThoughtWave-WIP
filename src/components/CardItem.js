9183e166ff6ce718dcf64d31388959f77d8a04ff
AJofEarth321 <103457615+AJofEarth321@users.noreply.github.com>
2022-06-18 16:11:58 -0400

    continue developing card components

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function CardItem() {
+function CardItem(props) {
   return (
     <>
         <li className='cards-item'>
-            <Link className='cards-item-link'>
-               <figure className='cards-item-pic-wrap'>
-                    <img src='/' alt=''
-                    className='cards-item-img'/>
+            <Link className='cards-item-link' to={props.path}>
+               <figure className='cards-item-pic-wrap' data-category={props.label}>
+                    <img src={props.src} alt='ThoughtWave'
+                    className='cards-item-img' />
                 </figure>
                 <div className='cards-item-info'>
-                    <h5 className='cards-item-text' />
+                    <h5 className='cards-item-text'>{props.text}</h5>
                 </div>  
             </Link>
         </li>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import CardItem from './CardItem';
 import './Cards.css';
+// import twGroup from '../images/twGroup';
 
 function Cards() {
   return (
     <div className='cards'>
-        <h1>Take a look!</h1>
+        <h1>Check out what makes the<br /> ThoughtWave approach so powerful!</h1>
         <div className='cards-container'>
             <div className='cards-wrapper'>
                 <ul className='cards-items'>
-                   {/* <CardItem 
-                   src='images/'
-                   text=""
-                   />  */}
+                   <CardItem 
+                   src={require('../images/twGroup.jpg')}
+                   text="Why Meditation Works"
+                   label='Explore'
+                   path="/impact"
+                /> 
                    
                 </ul>
             </div>
