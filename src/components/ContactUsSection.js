9a4fa916695a1411ae350b61e212653c91c31ed3
AJofEarth321 <103457615+AJofEarth321@users.noreply.github.com>
2022-07-06 21:13:27 -0400

    begin developing the contact us section

--- src/components/ContactUsSection.js
+++ src/components/ContactUsSection.js
@@ -3,8 +3,43 @@ import './ContactUsSection.css';
 
 function ContactUsSection() {
   return (
-    <div className='contact-us-container'>
-        <h1>CONTACT US</h1>
+    <div className='form-content-right'>
+      <form className='form'>
+        <h1>Contact us by filling out the information below.</h1>
+        <div className='form-inputs'>
+          <label htmlFor='firstname' className='form-label'>
+            First name
+            <input
+            id='firstname'
+            type='text'
+            name='firstname' 
+            className='form-input'
+            placeholder='enter your first name' />
+          </label>
+        </div>
+        <div className='form-inputs'>
+          <label htmlFor='lastname' className='form-label'>
+            Last name
+            <input
+            id='lastname'
+            type='text'
+            name='lastname' 
+            className='form-input'
+            placeholder='enter you last name' />
+          </label>
+        </div>
+        <div className='form-inputs'>
+          <label htmlFor='email' className='form-label'>
+            Email
+            <input
+            id='email'
+            type='text'
+            name='email' 
+            className='form-input'
+            placeholder='enter your email' />
+          </label>
+        </div>
+      </form>
     </div>
   )
 }
