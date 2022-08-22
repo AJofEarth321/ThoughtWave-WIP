2749cef8acd51d14357c0d2881283a5954088dcd
AJofEarth321 <103457615+AJofEarth321@users.noreply.github.com>
2022-08-22 14:57:27 -0400

    include documenation in the ScrollToTop file

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
+//this function enables automatic scroll to top of page upon page/link navigation
+
 function ScrollToTop () {
 
     const { pathname } = useLocation();
